084b74028aabd4c6dfe08d63e51d0edd31ea17b6
Rwin <rwinzhao@gmail.com>
2022-12-19 21:33:26 +1300

    change serve world

--- components/ServeWorld.jsx
+++ components/ServeWorld.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const ServeWorld = () => {
   return (
-    <div className="  bg-home-serve-texture bg-cover bg-center    flex justify-center relative h-[500px] bg-fixed">
+    <div className="  bg-home-serve-texture bg-cover bg-center bg-no-repeat  flex justify-center relative h-[350px] lg:h-[500px] bg-fixed">
         {/* <Image 
             alt="serveworld"
             src='https://media.graphassets.com/7X80fnSIS56yUzsB37tN'
@@ -11,12 +11,10 @@ const ServeWorld = () => {
             className="absolute  object-cover opacity-60"
         /> */}
         {/* <div className=" bg-color-btn absolute"></div> */}
-        <div className="absolute -top-16 bg-white rounded-lg  space-y-8 p-10 opacity-60 justify-center  mx-20 lg:w-80">
-            <h4 className=' text-color-text-header text-3xl'>我们想为我们周围的世界服务</h4>
-            <p>
-            圣灵所结的果子，就是仁爱、喜乐、和平、忍耐、恩慈、良善、信实、温柔、节制。这样的事没有律法禁止。 《加拉太书》第5章22-23节
-
-
+        <div className="absolute h-[60%] w-[90%] top-1/4 bg-white text-color-text-primary  font-bold rounded-lg  border-b-8 border-color-btn shadow-2xl  space-y-8  p-8 opacity-60  text-center lg:w-[50%] lg:h-[60%]">
+            <h5 className=' text-color-text-header text-2xl'>我们想为周围的世界服务</h5>
+            <p className=" font-bold">
+              圣灵所结的果子，就是仁爱、喜乐、和平、忍耐、恩慈、良善、信实、温柔、节制。这样的事没有律法禁止。 《加拉太书》第5章22-23节
             </p>
         </div>
     </div>
